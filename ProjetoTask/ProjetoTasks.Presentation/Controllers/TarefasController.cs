c20cf0967c40617c0c7197d9be7b9581d5647e15
André Rodrigues <andre.rodriguesneves@gmail.com>
2021-10-14 17:23:02 -0300

    Atualização
    Adição das paginas de cadastro

---
+++ ProjetoTask/ProjetoTasks.Presentation/Controllers/TarefasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTask.Presentation.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
